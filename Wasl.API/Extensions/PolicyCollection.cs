da94197756f6ed74680edd37b9f3eb874380cb2d
Default <default@email.com>
2026-05-08 23:15:22 +0300

    .

--- Wasl.API/Extensions/PolicyCollection.cs
+++ Wasl.API/Extensions/PolicyCollection.cs
@@ -11,20 +11,35 @@ namespace Wasl.API.Extensions
                  .RemoveServerHeader()
                 .AddPermissionsPolicy(builder =>
                 {
-                     builder.AddCamera().None();
-                     builder.AddMicrophone().None();
-                     builder.AddGeolocation().None();
+                    builder.AddCamera().None();
+                    builder.AddMicrophone().None();
+                    builder.AddGeolocation().None();
                 });
 
             if (app.Environment.IsProduction())
             {
-                policy.AddStrictTransportSecurity(31536000,true ,true)
+                policy.AddStrictTransportSecurity(31536000, true, true)
                       .AddContentSecurityPolicy(csp =>
                       {
+
                           csp.AddDefaultSrc().Self();
-                          csp.AddScriptSrc().Self();
-                          csp.AddStyleSrc().Self();
+
+
                           csp.AddImgSrc().Self().From("data:");
+                          csp.AddFontSrc().Self().From("data:");
+
+
+                          csp.AddConnectSrc().Self();
+
+
+                          csp.AddScriptSrc()
+                             .Self()
+                             .UnsafeInline()
+                             .UnsafeEval();
+
+                          csp.AddStyleSrc()
+                             .Self()
+                             .UnsafeInline();
                       });
             }
 
